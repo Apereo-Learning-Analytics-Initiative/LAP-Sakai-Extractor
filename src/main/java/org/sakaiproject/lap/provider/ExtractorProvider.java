361305a2016da17ca483b12e079102827419a4dc
Aaron Zeckoski <azeckoski@gmail.com>
2014-07-27 06:49:10 -0400

    POM file fixes License header update

--- src/main/java/org/sakaiproject/lap/provider/ExtractorProvider.java
+++ src/main/java/org/sakaiproject/lap/provider/ExtractorProvider.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2008 Sakaiproject Licensed under the
+ * Copyright 2014 Sakaiproject Licensed under the
  * Educational Community License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may
  * obtain a copy of the License at
@@ -12,7 +12,6 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
 package org.sakaiproject.lap.provider;
 
 import java.util.HashMap;
